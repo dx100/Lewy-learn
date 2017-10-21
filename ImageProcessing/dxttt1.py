434027964cdf1b79260e243c0281056d254aa5c0
dx100cam <dx100@cam.ac.uk>
2017-10-21 16:56:36 +0100

    Intermittent change

--- ImageProcessing/dxttt1.py
+++ ImageProcessing/dxttt1.py
@@ -12,3 +12,25 @@ misc.imsave('face.png', f) # uses the Image module (PIL)
 import matplotlib.pyplot as plt
 plt.imshow(f)
 plt.show()
+
+import numpy as np
+
+face = misc.face(gray=True)
+face[0, 40]
+
+# Slicing
+#face[10:13, 20:23]
+
+
+
+#face[100:120] = 255
+
+lx, ly = face.shape
+X, Y = np.ogrid[0:lx, 0:ly]
+mask = (X - lx / 2) ** 2 + (Y - ly / 2) ** 2 > lx * ly / 4
+# Masks
+face[mask] = 0
+# Fancy indexing
+#face[range(400), range(400)] = 255
+
+plt.imshow(face,cmap=plt.cm.gray)
